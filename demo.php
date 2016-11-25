61316fef6dbe536717b1868f5fcfb1a1b0747b46
陈树金 <982991805@qq.com>
2016-11-26 00:18:36 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,31 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<style type="text/css">
-		.wrap{
-			width: 200px;
-			height: 100px;
-			background-color: red;
-		}
-	</style>
-	<script src="angular.min.js"></script>
-</head>
-<body>
-	<div ng-app="myApp" ng-controller="myController">
-		<div class="wrap" ng-click="clickFn()"></div>
-	</div>
-</body>
-<script type="text/javascript">
-	var myApp=angular.module("myApp",[]);
-	myApp.controller("myController",function($scope,$http){
-		$scope.clickFn=function(){
-			// console.log("ok");
-			$http.get("demo%20v10%20start.php").success(function(response){
-				console.log(response);
-			});
-		}
-	});
-</script>
-</html>
+<?
+	echo '{"inf":"ok"}';
+?>
